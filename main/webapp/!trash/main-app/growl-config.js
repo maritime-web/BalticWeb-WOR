335c4cb5a7d4ac36ec27e4d424863b58d367443c
Roland <roland.beeres@gmail.com>
2017-01-30 18:42:37 +0100

    Cleaned directory
    Compact version

--- main/webapp/!trash/main-app/growl-config.js
+++
@@ -1,4 +0,0 @@
-angular.module('maritimeweb.app').config(['growlProvider', function (growlProvider) {
-    growlProvider.globalTimeToLive(8000);
-    growlProvider.globalPosition('bottom-right');
-}]);
